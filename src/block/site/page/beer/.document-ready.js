a0005f30bfe6b234481649007f1b53e4572e3835
caezar47 <iss47@ya.ru>
2016-02-29 15:24:32 +0300

    edit menu page v.0.0.3

--- src/block/site/page/beer/.document-ready.js
+++
@@ -1,4 +0,0 @@
-$("[data-toggle-nav]").click(function() {
-	var toggle_el = $(this).data("toggle-nav"); 
-	$(toggle_el).toggleClass("open-menu-navbar");
-});
--- src/block/site/page/menu/.document-ready.js
+++ src/block/site/page/menu/.document-ready.js
@@ -1,4 +1,4 @@
 $("[data-toggle-nav]").click(function() {
 	var toggle_el = $(this).data("toggle-nav"); 
-	$(toggle_el).toggleClass("open-menu-navbar");
+	$(toggle_el).toggleClass("open-menu-navbar"); 
 });
