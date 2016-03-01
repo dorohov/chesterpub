223f3963d4c97f263d96d214283476108b42551b
caezar47 <iss47@ya.ru>
2016-03-01 12:11:04 +0300

    edit index page v.0.0.3

--- src/block/site/page/menu/.document-ready.js
+++ src/block/site/page/menu/.document-ready.js
@@ -1,3 +1,7 @@
+$("[data-toggle-nav]").click(function() {
+	var toggle_el = $(this).data("toggle-nav"); 
+	$(toggle_el).toggleClass("open-menu-navbar"); 
+});
 $(".editDev__item").click(function() {
 	$(this).toggleClass("active"); 
 });
