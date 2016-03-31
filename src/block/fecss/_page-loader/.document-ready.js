b0e829770e739c0707fe81dcbf93a212d5ffb47a
caezar47 <iss47@ya.ru>
2016-03-31 13:12:29 +0300

    edit

--- src/block/fecss/_page-loader/.document-ready.js
+++ src/block/fecss/_page-loader/.document-ready.js
@@ -4,5 +4,7 @@
 		$('.page-loader').removeClass('active');
 	});
 	$(window).load(function(event){
+		var bl = $('div.page-loader.full-screen.chester-style .blind');
+		while(parseInt(bl.attr('data-pos')) < 100) {}
 		$('.page-loader').removeClass('active');
 	});
