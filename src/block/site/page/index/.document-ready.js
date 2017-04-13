e6902987a937aad13c9cbcf1019e5e62965c2126
azbn <i@azbn.ru>
2017-04-13 14:14:48 +0300

    201704131414

--- src/block/site/page/index/.document-ready.js
+++ src/block/site/page/index/.document-ready.js
@@ -16,11 +16,13 @@ $('._ibm__right-col').hover(
 );
 
 
+/*
 $('._azbn_contact-form-block, ._azbn_contact-get-form').on('click.not-_azbn_contact-form-block', function(event){
 	event.preventDefault();
 	event.stopPropagation();
 	//я не помню, зачем я это сделал, но сделал я это не случайно
 });
+*/
 
 $('body').on('click.not-_azbn_contact-form-block', function(event){
 	//event.preventDefault();
